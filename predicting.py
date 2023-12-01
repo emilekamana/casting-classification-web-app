48cc9ff687861df7989d553e630a9607a7e968ad
emilekamana <emileizere@gmail.com>
2023-12-01 16:06:00 +0200

    bug fixing

--- predicting.py
+++ predicting.py
@@ -2,14 +2,18 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
+import tensorflow as tf
+from keras.applications import ResNet152V2
+from keras.applications.resnet_v2 import preprocess_input
 import tensorflow.keras as keras
 from keras.models import load_model
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.preprocessing import StandardScaler
 
 
 # Function for image preprocessing
 # Define the image size for preprocessing
 img_size = (300, 300)
-
 
 def preprocess_image(image_path):
     img = Image.open(image_path)
@@ -18,6 +22,15 @@ def preprocess_image(image_path):
     img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
     return img_array
 
+
+def extract_features(image_path, baseModel):
+    image_array = preprocess_image(image_path)
+    preprocessed_images = preprocess_input(image_array)
+
+    # Extract features using ResNet50
+    features = baseModel.predict(preprocessed_images)
+
+    return features
 
 # Function for making predictions
 def predict_defect(image_array, model):
@@ -31,10 +44,41 @@ def predict_defect(image_array, model):
         predicted_label = "ok"
         return "The image is " + predicted_label
 
+# Function to check if an image is relevant based on clustering
+def is_relevant_by_cosine_similarity(image_path, baseModel, scaler, train_features_std):
+    test_features=extract_features(image_path, baseModel)
+
+    # Standardize features
+    image_features_std = scaler.transform(test_features)
+
+    similarities = cosine_similarity(image_features_std, train_features_std)
+    max_similarity = np.max(similarities)
+    average_similarity = np.mean(similarities)
+    min_similarity = np.min(similarities)
+    median_similarity = np.median(similarities)
+
+    print("average similarity: ", average_similarity)
+    print("max similarity: ", max_similarity)
+    print("min similarity: ", min_similarity)
+    print("median similarity: ", median_similarity)
+
+    # You can adjust the threshold as needed based on your dataset
+    if max_similarity >= 0.90:
+        return True
+    else:
+        return False
 
 # Main Streamlit app
 def main():
     st.title("Defect Casting Prediction app")
+
+    baseModel = ResNet152V2(weights='imagenet', include_top=False, pooling='avg')
+
+    scaler = StandardScaler()
+
+    train_features = np.load('train_features.npy')
+
+    train_features_std = scaler.fit_transform(train_features)
 
     # Upload image through Streamlit
     # Allow jpg, png, and jpeg file types
@@ -48,18 +92,21 @@ def main():
                 # Display the uploaded image
                 st.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
 
-                # Preprocess the image
-                processed_image = preprocess_image(uploaded_file)
+                #Check if image is relevant to the dataset
+                if is_relevant_by_cosine_similarity(uploaded_file, baseModel, scaler, train_features_std):
+                    # Preprocess the image
+                    processed_image = preprocess_image(uploaded_file)
 
-                # Load the saved model
-                model = load_model("best_fine_tuned_model.h5")
+                    # Load the saved model
+                    model = load_model("best_fine_tuned_model.h5")
 
-                # Make predictions
-                prediction = predict_defect(processed_image, model)
+                    # Make predictions
+                    prediction = predict_defect(processed_image, model)
 
-                # Display prediction result
-                st.success("Prediction: " + prediction)
-
+                    # Display prediction result
+                    st.success("Prediction: " + prediction)
+                else:
+                    st.error("The file you uploaded is not a valid type. Please upload a valid image file.")
             except:
                 # If the file is not an image, show an error message
                 st.error("The file you uploaded is not a valid type. Please upload a valid image file.")
